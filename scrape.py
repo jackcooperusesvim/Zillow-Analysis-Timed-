c9a96ea368091e36cd98795604e75a60c912529f
Dedé <de@Dedes-Air.lan>
2024-03-18 19:35:26 -0400

    getting somewhere - 3 playthroughs of The Most Hated by Polyphia

--- scrape.py
+++ scrape.py
@@ -1,32 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 states = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
-driver = webdriver.Chrome()
-driver.get("https://www.zillow.com/")
-time.sleep(1)
-search_box = driver.find_element(By.CSS_SELECTOR,'input[placeholder="Enter an address, neighborhood, city, or ZIP code"]') 
-search_box.send_keys(states[0])
-
-search_box.send_keys(Keys.ENTER)
-time.sleep(5)
+# This is just a file with my zip code, which is in gitignore
+with open("myzip.txt") as file:
+    myzip = file.readlines()
 
 
-search_box = driver.find_element(By.CSS_SELECTOR,'input[placeholder="Add another location"]') 
-next = driver.find_element(By.CSS_SELECTOR,'a[title="Next page"]')
-reset = driver.find_element(By.CSS_SELECTOR,'button[aria-label="Remove Tag"]')
-next.click()
-time.sleep(5)
-reset.click()
-time.sleep(5)
-search_box.send_keys(states[1])
-search_box.send_keys(Keys.ENTER)
-time.sleep(5)
+driver = webdriver.Safari()
+driver.get("https://www.redfin.com/zipcode/89052")
+input("enter once loaded")
 
 
+elem = driver.find_element(By.XPATH, "//*")
+source_code = elem.get_attribute("outerHTML")
 
+with open("index.html","wb") as file:
+    file.write(source_code.encode('utf-8'))
 
 
 # for input in states:
