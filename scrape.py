6f8511eb801c3854a0eff96ac287dc903117b3e5
Dedé <de@Dedes-Air.lan>
2024-03-19 05:46:54 -0400

    my crush has a bf, so today i code from 2-6 ig

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
 import time
 states = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
 
@@ -10,15 +12,37 @@ with open("myzip.txt") as file:
 
 driver = webdriver.Safari()
 driver.get("https://www.redfin.com/zipcode/89052")
-input("enter once loaded")
+time.sleep(2)
 
-
+print("Checking for code")
 elem = driver.find_element(By.XPATH, "//*")
 source_code = elem.get_attribute("outerHTML")
 
-with open("index.html","wb") as file:
-    file.write(source_code.encode('utf-8'))
+if source_code == None:
+    raise Exception("The code is a lie")
 
+print("Parsing HTML")
+parsed_html = BeautifulSoup(source_code.encode("utf-8"),"lxml")
+
+print("Writing HTML")
+with open("index.html","w") as file:
+    file.write(parsed_html.prettify())
+
+print("looking for HomeCardsContainer flex flex-wrap")
+
+#Remove the Ads
+
+HCsC = []
+i=1
+while True:
+    parsed_div = parsed_html.select_one("div[id~=MapHomeCard_{}]".format(i))
+    if parsed_div == None:
+        break
+    HCsC.append(parsed_div)
+    i+=1
+print(HCsC)
+print("Found {} Homes on this page".format(i))
+input()
 
 # for input in states:
 #     search_box = driver.find_element(By.CSS_SELECTOR,'input[placeholder="Add another location"]') 
